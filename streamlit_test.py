504c4beaa19587b01ee0a23896a609e29ad742fe
YSYK-KKNM <y2025zb@berkeley.edu>
2025-08-12 02:11:09 +0800

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -312,7 +312,7 @@ elif page=="b6":
     plt.ylabel('Scaled Temperature (Fahrenheit)', fontsize=12)
     plt.grid(alpha=0.3)
     st.pyplot(plt)
-     st.write("The Correlation Coefficient for CO₂ Emissions and Each Type of Natural Disaster")
+    st.write("The Correlation Coefficient for CO₂ Emissions and Each Type of Natural Disaster")
     corrs = {}
     gm=co2[(co2['Country']=='Germany')&(co2['Year'].between(1900,2024))]
     codi=pd.merge(disaster,gm,on='Year',how='inner')
